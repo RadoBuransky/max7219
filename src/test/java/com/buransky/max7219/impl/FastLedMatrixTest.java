6b97d29e7b76cbbc62aa7de54748d7f0505da0af
Rado Buransky <radoburansky@gmail.com>
2020-04-21 09:09:07 +0200

    FastLedMatrixTest

--- src/test/java/com/buransky/max7219/impl/FastLedMatrixTest.java
+++ src/test/java/com/buransky/max7219/impl/FastLedMatrixTest.java
@@ -1,10 +1,14 @@
 package com.buransky.max7219.impl;
 
+import com.buransky.max7219.Register;
+import com.buransky.max7219.register.ShutdownRegister;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 public class FastLedMatrixTest {
     private static final int DISPLAY_ROWS = 8;
@@ -82,7 +86,7 @@ public class FastLedMatrixTest {
 
         // Assert
         Assert.assertNotNull(result);
-        Assert.assertEquals(194, result.size());
+        assertExpectedResultSize(1, 4, result.size());
     }
 
     @Test
@@ -97,6 +101,52 @@ public class FastLedMatrixTest {
 
         // Assert
         Assert.assertNotNull(result);
-        Assert.assertEquals(1552, result.size());
+        assertExpectedResultSize(8, 4, result.size());
+    }
+
+    @Test
+    public void testExecuteAll() {
+        // Prepare
+        final Register register = ShutdownRegister.NormalOperation;
+
+        // Execute
+        final List<Byte> result = fastLedMatrix.executeAll(register);
+
+        // Assert
+        Assert.assertNotNull(result);
+        assertExpectedResultSize(1, 4, result.size());
+    }
+
+    @Test
+    public void testGetLedStatus() {
+        // Prepare
+        fastLedMatrix.setLedStatus(0, 0, true);
+
+        // Execute
+        final boolean result = fastLedMatrix.getLedStatus(0, 0);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void testTurnOffLed() {
+        // Prepare
+        fastLedMatrix.setLedStatus(0, 0, true);
+        fastLedMatrix.draw();
+        assertTrue(fastLedMatrix.getLedStatus(0, 0));
+
+        // Execute
+        fastLedMatrix.setLedStatus(0, 0, false);
+        final List<Byte> result = fastLedMatrix.draw();
+
+        // Assert
+        Assert.assertNotNull(result);
+        assertExpectedResultSize(1, 4, result.size());
+    }
+
+    private void assertExpectedResultSize(final int expectedStepsCount, final int displayCount, final int resultSize) {
+        Assert.assertEquals(expectedStepsCount*(displayCount*16*3 + 2), resultSize);
+
     }
 }
